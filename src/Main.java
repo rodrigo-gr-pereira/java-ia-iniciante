8f93693bf2718294064f80adbe9d828036e3f6bd
Rodrigo G Pereira <rodrigogr.pereira@gmail.com>
2025-06-20 22:04:41 -0300

    Padrões de desenvolvimento e conceitos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    private final  static String WELCOME_MESSAGE = "Olá informe o seu nome?";
+
+    public static void main(String[] args) {
+
+        //não vai parar o breakpoint;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(WELCOME_MESSAGE);
+        String name = scanner.next();
+        System.out.println("Informe sua idade?");
+        String age = scanner.next();
+        System.out.printf("Olá %s sua idade é %s.\n",name, age);
+
     }
 }
---
+++ src/br/com/dio/Aclass.java
@@ -0,0 +1,8 @@
+package br.com.dio;
+
+import java.util.Scanner;
+
+public class Aclass {
+}
+
+
