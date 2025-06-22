16851121ab59393560512d8717d2c807f50c9bd8
Rodrigo G Pereira <rodrigogr.pereira@gmail.com>
2025-06-22 20:29:16 -0300

    Padroes de desvonvimento e conceitos

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,35 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    private final  static String WELCOME_MESSAGE = "Olá informe o seu nome?";
 
     public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        //System.out.println("Quanto é 2 + 2?");
+        System.out.println("Quantos anos voce tem?");
+        var age = scanner.nextInt();
+        System.out.println("Você é emancipado?");
+        var isEmancipated = scanner.nextBoolean();
+        //var isRight = result == 4;
+        //var isWrong = result != 4;
+        var canDrive = age >= 18  || isEmancipated && age >=16;
+        //System.out.printf("O resultado é 4, você acertou? (%s) \n", );//isRight !isWrong
+            System.out.printf("Você pode dirigir? (%s) \n", canDrive);
 
-        //não vai parar o breakpoint;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(WELCOME_MESSAGE);
-        String name = scanner.next();
-        System.out.println("Informe sua idade?");
-        String age = scanner.next();
-        System.out.printf("Olá %s sua idade é %s.\n",name, age);
-
+            System.out.printf("true & true = s% \n", true & true);
+            System.out.printf("false & false = s% \n", false & false);
+            System.out.printf("true & false = s% \n", true & false);
+            System.out.printf("false & true = s% \n", false & true);
+            System.out.println("========================================");
+            System.out.printf("true || true = s% \n", true || true);
+            System.out.printf("false || false = s% \n", false || false);
+            System.out.printf("true || false = s% \n", true || false);
+            System.out.printf("false || true = s% \n", false || true);
+            System.out.println("========================================");
+            System.out.printf("!true = %s \n", !true);
+            System.out.printf("!fasle = %s \n", !false);
     }
 }
+
+
+
