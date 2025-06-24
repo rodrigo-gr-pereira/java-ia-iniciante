8a3577751bbb0724e6fd6bac9a3cb37daf7bf9b9
Rodrigo G Pereira <rodrigogr.pereira@gmail.com>
2025-06-23 21:21:02 -0300

    operadores aritmeticos

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-        //System.out.println("Quanto é 2 + 2?");
-        System.out.println("Quantos anos voce tem?");
-        var age = scanner.nextInt();
-        System.out.println("Você é emancipado?");
-        var isEmancipated = scanner.nextBoolean();
-        //var isRight = result == 4;
-        //var isWrong = result != 4;
-        var canDrive = age >= 18  || isEmancipated && age >=16;
-        //System.out.printf("O resultado é 4, você acertou? (%s) \n", );//isRight !isWrong
-            System.out.printf("Você pode dirigir? (%s) \n", canDrive);
-
-            System.out.printf("true & true = s% \n", true & true);
-            System.out.printf("false & false = s% \n", false & false);
-            System.out.printf("true & false = s% \n", true & false);
-            System.out.printf("false & true = s% \n", false & true);
-            System.out.println("========================================");
-            System.out.printf("true || true = s% \n", true || true);
-            System.out.printf("false || false = s% \n", false || false);
-            System.out.printf("true || false = s% \n", true || false);
-            System.out.printf("false || true = s% \n", false || true);
-            System.out.println("========================================");
-            System.out.printf("!true = %s \n", !true);
-            System.out.printf("!fasle = %s \n", !false);
+       var scanner = new Scanner(System.in);
+       System.out.println("Informe o primeiro numero:");
+       var value1 = scanner.nextInt();
+        System.out.println("Informe o segundo numero:");
+        var value2 = scanner.nextInt();
+        System.out.printf("%s * %s = %s \n", value1, value2, value1 % value2);
     }
 }
 
