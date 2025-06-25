a3bfa94a06c9c869abf3c198a13c53b5b17fb988
Rodrigo G Pereira <rodrigogr.pereira@gmail.com>
2025-06-25 17:29:57 -0300

    Exercicios

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,60 @@
+import java.time.OffsetDateTime;
 import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
-       var scanner = new Scanner(System.in);
-       System.out.println("Informe o primeiro numero:");
-       var value1 = scanner.nextInt();
-        System.out.println("Informe o segundo numero:");
-        var value2 = scanner.nextInt();
-        System.out.printf("%s * %s = %s \n", value1, value2, value1 % value2);
+
+        /*Ex1:
+        var baseYear = OffsetDateTime.now().getYear();
+        var scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        var name = scanner.next();
+        System.out.println("Digite  seu ano de nascimento: ");
+        var year = scanner.nextInt();
+        var age = baseYear - year;
+        System.out.printf("olá %s, você tem %s anos! \n", name, age);
+         */
+
+        /*Ex2
+        var scanner = new Scanner(System.in);
+        System.out.println("Calcule a area do quadrado!");
+        System.out.println("Digite  o tamanho do lado:");
+        var lado = scanner.nextInt();
+        var area = lado * lado;
+        System.out.printf("A aréa do quadrado é %s.\n", area);
+         */
+
+        /*Ex3
+        var scanner = new Scanner(System.in);
+        System.out.println("===============================");
+        System.out.println("Calculo da aréa de um retângulo");
+        System.out.println("===============================");
+        System.out.println("Digite a altura do retângulo:");
+        var altura = scanner.nextInt();
+        System.out.println("Digite a base do retângulo:");
+        var base = scanner.nextInt();
+        var area = base * altura;
+        System.out.printf("Há aréa do retângulo é %s. \n", area);
+
+         */
+        /*
+        var scanner = new Scanner(System.in);
+        System.out.println("============================");
+        System.out.println("Diferença de idade entre 2 pessoas.");
+        System.out.println("===============================");
+        System.out.println("Digite seu nome:");
+        var name1 = scanner.next();
+        System.out.println("Digite sua idade:");
+        var year1 = scanner.nextInt();
+        System.out.println("Digite seu nome:");
+        var name2 = scanner.next();
+        System.out.println("Digite sua idade:");
+        var year2 = scanner.nextInt();
+        var dYear = year1 - year2;
+        System.out.printf("Há diferença de idade entre %s e a %s e igual a %s ano. \n", name1,name2,dYear);
+         */
     }
 }
 
