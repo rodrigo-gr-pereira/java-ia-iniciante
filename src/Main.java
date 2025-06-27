4c0c8e3fdc840ec03f6f20fed3f5c3bb45e5b16e
Rodrigo G Pereira <rodrigogr.pereira@gmail.com>
2025-06-26 22:37:49 -0300

    Estrutura condicional if else e else if

--- src/Main.java
+++ src/Main.java
@@ -5,58 +5,20 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        /*Ex1:
-        var baseYear = OffsetDateTime.now().getYear();
         var scanner = new Scanner(System.in);
-        System.out.println("Digite seu nome: ");
+        System.out.println("Informe seu nome: ");
         var name = scanner.next();
-        System.out.println("Digite  seu ano de nascimento: ");
-        var year = scanner.nextInt();
-        var age = baseYear - year;
-        System.out.printf("olá %s, você tem %s anos! \n", name, age);
-         */
+        System.out.println("Informe sua idade: ");
+        var age = scanner.nextInt();
+        System.out.println("Você é emancipado? (s/n)");
+        var isEmancipated = scanner.next().equalsIgnoreCase("s");
 
-        /*Ex2
-        var scanner = new Scanner(System.in);
-        System.out.println("Calcule a area do quadrado!");
-        System.out.println("Digite  o tamanho do lado:");
-        var lado = scanner.nextInt();
-        var area = lado * lado;
-        System.out.printf("A aréa do quadrado é %s.\n", area);
-         */
-
-        /*Ex3
-        var scanner = new Scanner(System.in);
-        System.out.println("===============================");
-        System.out.println("Calculo da aréa de um retângulo");
-        System.out.println("===============================");
-        System.out.println("Digite a altura do retângulo:");
-        var altura = scanner.nextInt();
-        System.out.println("Digite a base do retângulo:");
-        var base = scanner.nextInt();
-        var area = base * altura;
-        System.out.printf("Há aréa do retângulo é %s. \n", area);
-
-         */
-        /*
-        var scanner = new Scanner(System.in);
-        System.out.println("============================");
-        System.out.println("Diferença de idade entre 2 pessoas.");
-        System.out.println("===============================");
-        System.out.println("Digite seu nome:");
-        var name1 = scanner.next();
-        System.out.println("Digite sua idade:");
-        var year1 = scanner.nextInt();
-        System.out.println("Digite seu nome:");
-        var name2 = scanner.next();
-        System.out.println("Digite sua idade:");
-        var year2 = scanner.nextInt();
-        var dYear = year1 - year2;
-        System.out.printf("Há diferença de idade entre %s e a %s e igual a %s ano. \n", name1,name2,dYear);
-         */
+        var canDrive = (age >= 18) || (age >=16 && isEmancipated);
+        if (canDrive) {
+            System.out.printf("%s, você pode dirigir \n", name, age);
+        } else {
+            System.out.printf("%s, você pode dirigir. \n", name);
+        }
     }
 }
-
-
 
