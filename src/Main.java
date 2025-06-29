bdf9879fe6a96a1953c3ccb47e76ffb8712cfb57
Rodrigo G Pereira <rodrigogr.pereira@gmail.com>
2025-06-29 20:54:49 -0300

    Estrutura condicional switch case

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,58 @@ public class Main {
 
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
-        System.out.println("Informe seu nome: ");
-        var name = scanner.next();
-        System.out.println("Informe sua idade: ");
-        var age = scanner.nextInt();
-        System.out.println("Você é emancipado? (s/n)");
-        var isEmancipated = scanner.next().equalsIgnoreCase("s");
+        System.out.println("Informe um número de 1 a 7:");
+        var option = scanner.nextInt();
+        /* Ex1
+        switch (option){
+            case 1:
+            case 7:
+                System.out.println("Final de semana! uhuhuh \\o//");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            default:
+                System.out.println("Opção inválida");
 
-        var canDrive = (age >= 18) || (age >=16 && isEmancipated);
-        if (canDrive) {
-            System.out.printf("%s, você pode dirigir \n", name, age);
-        } else {
-            System.out.printf("%s, você pode dirigir. \n", name);
         }
+         Ex2:
+        switch (option){
+            case 1, 7-> System.out.println("Final de semana! \\o/");
+            case 2 -> System.out.println("Segunda");
+            case 3 -> System.out.println("Terça");
+            case 4 -> System.out.println("Quarta");
+            case 5 -> System.out.println("Quinta");
+            case 6 -> System.out.println("Sexta");
+            default -> System.out.println("Opção inválida");
+        }
+
+         */
+       var message = switch (option){
+           case 1,7 -> {
+               var day = option == 1 ? "Domingo" : "Sábado";
+               yield String.format("Hoje é %s, fim de semana uhuul \\o/", day);
+           }
+           case 2 -> "Segunda";
+           case 3 -> "Terça";
+           case 4 -> "Quarta";
+           case 5 -> "Quinta";
+           case 6 -> "Sexta";
+           default -> "Opção inválida";
+       };
+       System.out.println(message);
+
     }
 }
-
