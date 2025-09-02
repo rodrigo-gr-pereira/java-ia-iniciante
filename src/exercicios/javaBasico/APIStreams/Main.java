2d419c09bac18862dc9340a4f1695fbe1a58637b
Rodrigo G Pereira <rodrigogr.pereira@gmail.com>
2025-09-01 22:27:03 -0300

    Intro Stream

--- src/exercicios/javaBasico/APIStreams/Main.java
+++ src/exercicios/javaBasico/APIStreams/Main.java
@@ -3,18 +3,32 @@ package exercicios.javaBasico.APIStreams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        List<Integer> values1 = List.of(3, 6, 9, 12);
+        List<Integer> values2 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
 
+        var newValues = values2.stream()
+                .filter(values1::contains)
+                .peek(n -> System.out.printf("Filter %s \n", n))
+                .map(n -> values1.stream().reduce(n, (n1, n2) -> n1 - n2))
+                .peek(n -> System.out.printf("Map %s \n", n))
+                .collect(Collectors.toSet());
+
+        System.out.println(newValues);
+
+
+        /* Ex3
         var value = List.of("Maria", "João", "Ana", "Pedro", "Lucas")
                 .stream()
                 .filter(name -> name.endsWith("a"))
                 .toList();
 
         System.out.println(value);
-        /*Ex2
+        Ex2
         // List<String> debugValues = new ArrayList<>();
        // var value = Stream.of("Maria", "João", "Ana", "Pedro", "Lucas")
               //  .peek(System.out::println)
